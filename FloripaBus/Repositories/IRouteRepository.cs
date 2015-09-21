96011e42575b31655461386586e3d0c4981a8a8a
Rodrigo Wirth <rodrigowirth90@gmail.com>
2015-09-21 12:27:26 -0300

    Adding communication to the REST api

--- FloripaBus/Repositories/IRouteRepository.cs
+++ FloripaBus/Repositories/IRouteRepository.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace FloripaBus
 {
@@ -15,11 +19,28 @@ namespace FloripaBus
 	{
 		public async Task<IList<Route>> FindRoutesByStopNameAsync (string stopName)
 		{
-			IList<Route> routes = new List<Route> ();
-			routes.Add(new Route(1, "110", "Coral"));
-			routes.Add(new Route(2, "223", "Centro - Direto"));
-			routes.Add(new Route(3, "171", "Brusque"));
+			string responseContent;
+			using (var client = new HttpClient ()) 
+			{
+				var request = new HttpRequestMessage(HttpMethod.Post, "https://api.appglu.com/v1/queries/findRoutesByStopName/run");
+				request.Headers.Add ("X-AppGlu-Environment", "staging");
+				request.Headers.Add("Authorization", "Basic V0tENE43WU1BMXVpTThWOkR0ZFR0ek1MUWxBMGhrMkMxWWk1cEx5VklsQVE2OA==");
 
+				var body = "{ \"params\": { \"stopName\": \"%" + stopName + "%\" } }";
+				var content = new StringContent(body, Encoding.UTF8, "application/json");
+				request.Content = content;
+
+				var response = await client.SendAsync(request);
+				responseContent = await response.Content.ReadAsStringAsync();
+			}
+
+			IList<Route> routes = new List<Route> ();
+			RoutesReponse data = JsonConvert.DeserializeObject<RoutesReponse>(responseContent);
+			foreach (var row in data.Rows)
+			{
+				routes.Add (new Route(row.Id, row.ShortName, row.LongName));
+			}
+				
 			return routes;
 		}
 
@@ -32,6 +53,20 @@ namespace FloripaBus
 		{
 			throw new NotImplementedException ();
 		}
+
+		private class RoutesReponse
+		{
+			public List<RouteRow> Rows {get;set;}
+		}
+
+		private class RouteRow
+		{
+			public int Id { get; set; }
+			public string ShortName { get; set; }
+			public string LongName { get; set; }
+		}
+
+
 	}
 }
 
